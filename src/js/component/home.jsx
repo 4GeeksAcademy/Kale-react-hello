6ddd068af255c4abfed6272c2d0d9c4d54e561cf
KaleCordero <kalecascante61@gmail.com>
2024-09-20 22:20:17 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		let contadorSegundo = setInterval(() => {
+			setCount(count + 1)
+		}, 1000)
+		return () => {
+			clearInterval(contadorSegundo)
+		}
+	}, [count])
+
+	const segundos = count.toString().padStart(6, '0').split('');
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="p-3 mb-2 bg-dark" style={{ display: 'flex', justifyContent: 'space-around', marginTop: "0.5rem", fontSize: "4rem", fontFamily: "serif" }}>
+			<div className="card text-white bg-dark" style={{ background: "", }}>
+				<div className="card-body">
+					<a><i class="fa-regular fa-clock"></i></a>
+				</div>
+			</div>
+			{segundos.map((digito, index) => (
+				<div key={index}>
+					<div className="card text-white bg-dark">
+						<div className="card-body">
+							{digito}
+						</div>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };
